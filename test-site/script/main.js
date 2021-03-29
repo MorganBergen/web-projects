366b35ad8165a6b59a061107e5e71ff1e48ebb3f
Morgan Bergen <morganmahabergen@icloud.com>
2021-03-29 13:29:16 -0500

    added an image changer to javascript

--- test-site/script/main.js
+++ test-site/script/main.js
@@ -1,6 +1,9 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Morgan Bergen';
-
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+        let mySrc = myImage.getAttribute('src');
+        if (mySrc === 'images/sphere1.png') {
+            myImage.setAttribute('src', 'images/sphere2.png');
+        } else {
+            myImage.setAttribute('src', 'images/sphere1.png');
+        }
 }
