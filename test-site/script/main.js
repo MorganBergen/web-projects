06665f04f5eb0c30d097d1320825e45b11fe4265
Morgan Bergen <morganmahabergen@icloud.com>
2021-03-29 14:37:45 -0500

    added functionality to main.js

--- test-site/script/main.js
+++ test-site/script/main.js
@@ -1,4 +1,6 @@
+
 let myImage = document.querySelector('img');
+
 myImage.onclick = function() {
         let mySrc = myImage.getAttribute('src');
         if (mySrc === 'images/sphere1.png') {
@@ -7,3 +9,22 @@ myImage.onclick = function() {
             myImage.setAttribute('src', 'images/sphere1.png');
         }
 }
+
+let myButton = document.querySelector('button');
+
+myButton.onclick = function() {
+    setUserName();
+  }
+
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Morgan is cool, ' + myName;
+    }
+}
+
