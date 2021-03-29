27b9aee284dc0372b2e1bc6497f21366c6c2a0b9
Morgan Bergen <morganmahabergen@icloud.com>
2021-03-29 12:23:56 -0500

    added click event for interactivity

--- test-site/script/main.js
+++ test-site/script/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Morgan Bergen';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
